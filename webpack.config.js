7081cdde6fc93b6d4d2039eab4f72980f966862f
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2019-05-22 19:52:07 +0100

    installed styled-components

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
         include: path.resolve(__dirname, 'src'),
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['babel-plugin-styled-components']
         }
       }
     ]
