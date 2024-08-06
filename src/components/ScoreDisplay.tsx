77aeefce4b1138b790ef6a539d926c7c4d3ccb4c
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-08-06 16:54:06 +0100

    src/components/ScoreDisplay.tsx: change label 'correct' to 'score'

--- src/components/ScoreDisplay.tsx
+++ src/components/ScoreDisplay.tsx
@@ -5,7 +5,7 @@ type Props = { score: Score };
 export function ScoreDisplay({ score: { attempts, correct } }: Props) {
 	return (
 		<dl className="flex items-center gap-2 self-end">
-			<dt>correct:</dt>
+			<dt>score:</dt>
 			<dd className="flex items-center gap-2 text-3xl font-bold">
 				{correct}
 				<span className="text-center text-xs">
