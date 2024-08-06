a3cc094c6b7da0068e15637c0b8acbd583530af9
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-08-06 14:51:00 +0100

    .eslintrc.cjs: add additional rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,5 +7,44 @@ module.exports = {
 	plugins: ["react-refresh"],
 	rules: {
 		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+		"no-restricted-syntax": [
+			"error",
+			{
+				selector: "TSEnumDeclaration",
+				message: "Use const assertion or a string union type instead.",
+			},
+		],
+		"@typescript-eslint/naming-convention": [
+			"error",
+			{
+				selector: "variable",
+				types: ["boolean"],
+				format: ["PascalCase"],
+				prefix: ["is", "should", "has", "can", "did", "will"],
+			},
+		],
+		"@typescript-eslint/naming-convention": [
+			"error",
+			{
+				// Generic type parameter must start with letter T, followed by any uppercase letter.
+				selector: "typeParameter",
+				format: ["PascalCase"],
+				custom: { regex: "^T[A-Z]", match: true },
+			},
+		],
+		/*
+		"jest/valid-title": [
+			"error",
+			{
+				mustMatch: { it: [/should.*when/u.source, "Test title must include 'should' and 'when'"] },
+			},
+		],
+		*/
 	},
+	overrides: [
+		{
+			files: ["src/pages/**/*"],
+			rules: { "import/no-default-export": "off" },
+		},
+	],
 };
