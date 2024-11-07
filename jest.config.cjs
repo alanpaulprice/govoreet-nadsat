d8d0668bed507fb4610eb77f7ea2eec9a905ad0d
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-11-07 16:18:39 +0000

    jest.config.cjs: add additional moduleNameMapper properties, for @components/hooks/modules/utilities

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
 	testEnvironment: "jsdom",
 	moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
 	moduleNameMapper: {
+		"^@components$": "<rootDir>/src/common/components",
 		"^@constants$": "<rootDir>/src/common/constants",
+		"^@hooks$": "<rootDir>/src/common/hooks",
+		"^@modules$": "<rootDir>/src/common/modules",
 		"^@types$": "<rootDir>/src/common/types",
+		"^@utilities$": "<rootDir>/src/common/utilities",
 	},
 };
